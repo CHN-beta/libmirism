07e1b0b307e306fde7d068d4b5004af918c35a3b
chn <chn@chn.moe>
2022-06-18 17:04:23 +0800

    html解析器（未完成）

---
+++ include/mirism/detail/content/text/html.hpp
@@ -0,0 +1,28 @@
+# pragma once
+# include <mirism/detail/logger.hpp>
+
+namespace mirism::content::text
+{
+	class Html
+	{
+		public: struct Attribute
+		{
+			std::string name;
+			std::optional<std::string> value;
+		};
+		public: struct Node
+		{
+			std::string Tag;
+			std::vector<Attribute> Attributes;
+			std::vector<std::variant<Node, std::string>> Children;
+			std::experimental::observer_ptr<Node> Parent;
+		};
+		public: struct Document
+		{
+			std::vector<std::string> Doctype;
+			std::vector<Node> Nodes;
+		};
+		public: std::optioinal<Document> deserialize(const std::string& s);
+		public: std::string serialize(const Node& node);
+	};
+}
